0bd31c0e7b41e7c703fbc6eaff59fe612c39c6bd
Alijimpa <alibadpa007@gmail.com>
2025-11-15 17:04:49 +0330

    Implement First Cpp Actor Class for Print Log

---
+++ Source/UE_StudyProject/GameDevTV/StudyActor.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StudyActor.h"
+
+// Sets default values
+AStudyActor::AStudyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AStudyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp,Display,TEXT("Hallolee"));
+	
+}
+
+// Called every frame
+void AStudyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
