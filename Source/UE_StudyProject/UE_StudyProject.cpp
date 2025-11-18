1078c0bd34bfbe682d23956a05acb94c1a5a7596
Alijimpa <alibadpa007@gmail.com>
2025-11-18 13:19:16 +0330

    Add FPS Template Cpp Code to Project & Fixed

--- Source/UE_StudyProject/UE_StudyProject.cpp
+++ Source/UE_StudyProject/UE_StudyProject.cpp
@@ -4,3 +4,5 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_StudyProject, "UE_StudyProject" );
+DEFINE_LOG_CATEGORY(LogUE_StudyProject)
+DEFINE_LOG_CATEGORY(LogTP_FirstPerson)
